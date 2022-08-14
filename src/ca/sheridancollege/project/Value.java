1e67f2c7e31f276f34152ca7a52ef0c98ac09860
Marshall Christie <105458045+HexHimself@users.noreply.github.com>
2022-08-14 12:39:07 -0400

    Add files via upload

--- src/ca/sheridancollege/project/Value.java
+++ src/ca/sheridancollege/project/Value.java
@@ -1,19 +1,27 @@
-package ca.sheridancollege.project;
-
 
 
 public enum Value {
     ACE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    JACK,
-    QUEEN,
-    KING
+    TWO (2),
+    THREE (3),
+    FOUR (4),
+    FIVE (5),
+    SIX (6),
+    SEVEN (7),
+    EIGHT (8),
+    NINE (9),
+    TEN (10),
+    JACK (10),
+    QUEEN (10),
+    KING (10);
+    
+    private final int numValue;
+    
+    Value(int numValue) {
+        this.numValue = numValue;
+    }
+    
+    private int getNumValue() {
+        return numValue;
+    }
 }
